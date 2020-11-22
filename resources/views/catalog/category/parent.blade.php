9feaf22473dfd6dab8ee4da2518e641b36ac08dd
arendach <arendach.taras@gmail.com>
2020-11-22 03:17:58 +0300

    fix parent page

--- resources/views/catalog/category/parent.blade.php
+++ resources/views/catalog/category/parent.blade.php
@@ -20,14 +20,6 @@
             </div>
 
             @foreach($category->child as $child)
-                {{-- <h2>{{ $child->name }}</h2>
-                 <div class="row small-gutters">
-                     @foreach($child->products as $product)
-                         <div class="col-6 col-md-4 col-xl-3">
-                             @include('catalog.parts.product-card')
-                         </div>
-                     @endforeach
-                 </div>--}}
 
                 <div class="container margin_60_35">
                     <h2 style="margin-bottom: 50px"><a href="{{ $child->url }}">{{ $child->name }}</a></h2>
@@ -47,8 +39,4 @@
             <div>{!! $category->description !!}</div>
         </div>
     </main>
-@endsection
-
-@section('js')
-
 @endsection
