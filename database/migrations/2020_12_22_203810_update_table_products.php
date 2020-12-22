c17bd40b4859c7789b8545fa7c572591ba5095cb
brendi211 <s5660max@gmail.com>
2020-12-22 20:51:39 +0200

    Fix sort products from nova

---
+++ database/migrations/2020_12_22_203810_update_table_products.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTableProducts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+
+       });
+        DB::statement('UPDATE `products` SET `order` = `id`');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
