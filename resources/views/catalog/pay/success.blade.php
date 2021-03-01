530ff1d23b222c939ff169efcf778a92818d50ee
brendi211 <s5660max@gmail.com>
2021-03-01 20:17:36 +0200

    fix order

--- resources/views/catalog/pay/success.blade.php
+++ resources/views/catalog/pay/success.blade.php
@@ -4,17 +4,13 @@
 
 @section('content')
     <main class="bg_gray">
-
         <div class="container margin_30">
             <div class="alert alert-success">
                 {{ translate('Оплата прошла успешно. Спасибо за заказ.') }}.
-                @if(!empty($orderFind->email))
-                    <div class="alert alert-info mt-4">
-                        {{ translate('Вам отправлена квитанция на указанный E-mail') }}.
-                    </div>
-                @endif
+                <div class="alert alert-info mt-4">
+                    {{ translate('Вам отправлена квитанция на указанный E-mail') }}.
+                </div>
             </div>
         </div>
-
     </main>
 @endsection
