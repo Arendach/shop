b8b560392438239c79ed340515e0a552adf45efe
brendi211 <s5660max@gmail.com>
2021-03-02 16:30:20 +0200

    fix order/Change collum liqpay_order_id

---
+++ database/migrations/2021_03_02_161953_change_liq_pay_order_id_table_order_pay.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeLiqPayOrderIdTableOrderPay extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_pay', function (Blueprint $table) {
+            $table->string('liqpay_order_id',255)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_pay', function (Blueprint $table) {
+            //
+        });
+    }
+}
