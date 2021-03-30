dc291c4486da5e585d9c333f561d665423b15136
brendi211 <s5660max@gmail.com>
2021-03-30 10:23:44 +0300

    fix default language2

--- config/locale.php
+++ config/locale.php
@@ -4,7 +4,7 @@ return [
     'default' => env('DEFAULT_LANGUAGE', 'ru'),
     'hide_default' => true,
     'support' => ['uk', 'ru'],
-    'current' => 'uk',
+    'current' => env('DEFAULT_LANGUAGE', 'ru'),
     'prefix' => '',
     'locale'
 ];
