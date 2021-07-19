9b2024bb318c4bdbbef8ab96ef22a5c7503ca87c
Pyskun <m.pyskun@vis-design.com>
2021-07-19 17:44:21 +0300

    test mail

--- app/Http/Controllers/StreamTele/MainController.php
+++ app/Http/Controllers/StreamTele/MainController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\StreamTele;
 use App\Http\Controllers\Controller;
 use App\StreamTele\Exceptions\AuthFailedException;
 use App\StreamTele\Sms\Auth;
+use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Order as sendMail;
 
 class MainController extends Controller
 {
@@ -15,6 +18,12 @@ class MainController extends Controller
 
     public function Test()
     {
+        $order = Order::first();
+        $mail = Mail::to('s5660max@gmail.com')->send(
+            new sendMail()
+        );
+        dd($mail, $order);
+
         try {
             $result = app(Auth::class)->smsSend( '0988580982');
             dd($result);
